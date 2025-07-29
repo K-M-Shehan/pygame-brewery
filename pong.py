6bca6f3d3b1a45cba789c414edb736a8be2e6970
K-M-Shehan <miweshshehan@gmail.com>
2025-07-29 16:04:30 +0530

    added collision for the box
    big victory today: removed the box and added 4 lines to form a box then added collision for each line and now the thing works!

--- pong.py
+++ pong.py
@@ -12,14 +12,25 @@ left_pong_pos = pygame.Vector2(80, 50)
 right_pong_pos = pygame.Vector2(880, 50) 
 ball_pos = pygame.Vector2(480, 240)
 
+box_up_pos = pygame.Vector2(20, 20)
+box_down_pos = pygame.Vector2(20, 456)
+box_right_pos = pygame.Vector2(936, 20)
+
 ball_velocity = pygame.Vector2(8, -2)
 
-box_rect = pygame.Rect(20, 20, 920, 440)
+#box_rect = pygame.Rect(20, 20, 920, 440)
+
+box_up_rect = pygame.Rect(box_up_pos, (920, 4))
+box_down_rect = pygame.Rect(box_down_pos, (920, 4))
+box_left_rect = pygame.Rect(box_up_pos, (4, 440))
+box_right_rect = pygame.Rect(box_right_pos, (4, 440))
 
 left_pong_rect = pygame.Rect(left_pong_pos, (15, 60))
 right_pong_rect = pygame.Rect(right_pong_pos, (15, 60))
 
 ball_rect = pygame.Rect(ball_pos, (10, 10))
+
+start_flag= False;
 
 while True:
     for event in pygame.event.get():
@@ -45,19 +56,42 @@ while True:
         right_pong_pos.x -= 5
     if keys[pygame.K_RIGHT]:
         right_pong_pos.x += 5
+    if keys[pygame.K_SPACE]:
+        start_flag = True
 
     ## ball movement
-    ball_pos += ball_velocity
+    if start_flag == True:
+        ball_pos += ball_velocity
+    
+    #if ball_rect.top <= box_rect.top:
+    #    ball_velocity.y *= -1
+    #    ball_rect.top = box_rect.top
+    #    ball_pos.y = ball_rect.top
+    
+    #if ball_rect.bottom >= box_rect.bottom:
+    #    ball_velocity.y *= -1
+    #    ball_rect.bottom = box_rect.bottom
+    #    ball_pos.y = ball_rect.top
 
-    if ball_rect.top <= box_rect.top:
+    if ball_rect.colliderect(box_up_rect):
         ball_velocity.y *= -1
-        ball_rect.top = box_rect.top
+        ball_rect.top = box_up_rect.bottom
         ball_pos.y = ball_rect.top
 
-    if ball_rect.bottom >= box_rect.bottom:
+    if ball_rect.colliderect(box_down_rect):
         ball_velocity.y *= -1
-        ball_rect.bottom = box_rect.bottom
+        ball_rect.bottom = box_down_rect.top
         ball_pos.y = ball_rect.top
+
+    if ball_rect.colliderect(box_right_rect):
+        ball_velocity.x *= -1
+        ball_rect.right = box_right_rect.left
+        ball_pos.x = ball_rect.left
+
+    if ball_rect.colliderect(box_left_rect):
+        ball_velocity.x *= -1
+        ball_rect.left = box_left_rect.right
+        ball_pos.x = ball_rect.left
 
     if ball_rect.colliderect(left_pong_rect) and ball_velocity.x < 0:
         ball_velocity.x *= -1
@@ -76,7 +110,13 @@ while True:
 
     # render shit
     screen.fill('black')
-    pygame.draw.rect(screen, 'white', box_rect, 4) # the border box
+    #pygame.draw.rect(screen, 'white', box_rect, 4) # the border box
+
+    pygame.draw.rect(screen, 'white', box_up_rect)
+    pygame.draw.rect(screen, 'white', box_right_rect)
+    pygame.draw.rect(screen, 'white', box_down_rect)
+    pygame.draw.rect(screen, 'white', box_left_rect)
+
     pygame.draw.rect(screen, 'white', left_pong_rect)
     pygame.draw.rect(screen, 'white', right_pong_rect)
     pygame.draw.circle(screen, 'white', ball_pos, 10)
